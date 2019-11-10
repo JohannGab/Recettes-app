876aafa62d3963b0302bb2fd4cf5beb6b7c7d295
JohannGab <johann.gabelle@outlook.fr>
2019-11-11 00:12:54 +0100

    first commit

---
+++ src/base.js
@@ -0,0 +1,17 @@
+import Rebase from 're-base'
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyDNtE3ApNwCsLk5q7HXk2dOvymxTaHHNDQ",
+    authDomain: "recettes-app-f19e5.firebaseapp.com",
+    databaseURL: "https://recettes-app-f19e5.firebaseio.com",
+})
+
+const base = Rebase.createClass(firebaseApp.database())
+
+// This is a named export
+export { firebaseApp }
+
+// this is a default export
+export default base
